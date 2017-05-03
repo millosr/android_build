darren-clark/android_platform_build

android-x86: add kernel building rules

core/tasks/kernel.mk
@@ -1,0 +1,107 @@
+#
+# Copyright (C) 2014 The Android-x86 Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+
+ifeq ($(KBUILD_OUTPUT),)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+KERNEL_DIR ?= kernel
+
+ifneq ($(filter x86%,$(TARGET_ARCH)),)
+TARGET_KERNEL_ARCH ?= $(TARGET_ARCH)
+KERNEL_TARGET := bzImage
+TARGET_KERNEL_CONFIG ?= android-$(TARGET_KERNEL_ARCH)_defconfig
+KERNEL_CONFIG_DIR := arch/x86/configs
+endif
+ifeq ($(TARGET_ARCH),arm)
+KERNEL_TARGET := zImage
+TARGET_KERNEL_CONFIG ?= goldfish_defconfig
+KERNEL_CONFIG_DIR := arch/arm/configs
+endif
+
+KBUILD_OUTPUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/kernel)
+mk_kernel := + $(hide) $(MAKE) -C $(KERNEL_DIR) O=$(KBUILD_OUTPUT) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(abspath $(TARGET_TOOLS_PREFIX)) $(if $(SHOW_COMMANDS),V=1)
+
+KERNEL_CONFIG_FILE := $(if $(wildcard $(TARGET_KERNEL_CONFIG)),$(TARGET_KERNEL_CONFIG),$(KERNEL_DIR)/$(KERNEL_CONFIG_DIR)/$(TARGET_KERNEL_CONFIG))
+
+MOD_ENABLED := $(shell grep ^CONFIG_MODULES=y $(KERNEL_CONFIG_FILE))
+FIRMWARE_ENABLED := $(shell grep ^CONFIG_FIRMWARE_IN_KERNEL=y $(KERNEL_CONFIG_FILE))
+
+# I understand Android build system discourage to use submake,
+# but I don't want to write a complex Android.mk to build kernel.
+# This is the simplest way I can think.
+KERNEL_DOTCONFIG_FILE := $(KBUILD_OUTPUT)/.config
+KERNEL_ARCH_CHANGED := $(if $(filter 0,$(shell grep -s ^$(if $(filter x86,$(TARGET_KERNEL_ARCH)),\#.)CONFIG_64BIT $(KERNEL_DOTCONFIG_FILE) | wc -l)),FORCE)
+$(KERNEL_DOTCONFIG_FILE): $(KERNEL_CONFIG_FILE) $(wildcard $(TARGET_KERNEL_DIFFCONFIG)) $(KERNEL_ARCH_CHANGED)
+	$(hide) mkdir -p $(@D) && cat $(wildcard $^) > $@
+	$(mk_kernel) oldnoconfig
+
+# bison is needed to build kernel and external modules from source
+BISON := $(HOST_OUT_EXECUTABLES)/bison$(HOST_EXECUTABLE_SUFFIX)
+
+BUILT_KERNEL_TARGET := $(KBUILD_OUTPUT)/arch/$(TARGET_ARCH)/boot/$(KERNEL_TARGET)
+$(INSTALLED_KERNEL_TARGET): $(KERNEL_DOTCONFIG_FILE) | $(ACP) $(BISON)
+	$(mk_kernel) $(KERNEL_TARGET) $(if $(MOD_ENABLED),modules)
+	$(hide) $(ACP) -fp $(BUILT_KERNEL_TARGET) $@
+	$(if $(FIRMWARE_ENABLED),$(mk_kernel) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) firmware_install)
+
+ifneq ($(MOD_ENABLED),)
+KERNEL_MODULES_DEP := $(firstword $(wildcard $(TARGET_OUT)/lib/modules/*/modules.dep))
+KERNEL_MODULES_DEP := $(if $(KERNEL_MODULES_DEP),$(KERNEL_MODULES_DEP),$(TARGET_OUT)/lib/modules)
+
+$(TARGET_OUT_INTERMEDIATES)/%.kmodule: $(INSTALLED_KERNEL_TARGET)
+	$(hide) cp -an $(EXTRA_KERNEL_MODULE_PATH_$*) $(TARGET_OUT_INTERMEDIATES)/$*.kmodule
+	@echo Building additional kernel module $*
+	$(mk_kernel) M=$(abspath $@) modules
+
+$(KERNEL_MODULES_DEP): $(INSTALLED_KERNEL_TARGET) $(patsubst %,$(TARGET_OUT_INTERMEDIATES)/%.kmodule,$(TARGET_EXTRA_KERNEL_MODULES))
+	$(hide) rm -rf $(TARGET_OUT)/lib/modules
+	$(mk_kernel) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) modules_install
+	+ $(hide) for kmod in $(TARGET_EXTRA_KERNEL_MODULES) ; do \
+	        echo Installing additional kernel module $${kmod} ; \
+	        $(subst +,,$(subst $(hide),,$(mk_kernel))) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/$${kmod}.kmodule modules_install ; \
+	done
+	$(hide) rm -f $(TARGET_OUT)/lib/modules/*/{build,source}
+endif
+
+$(BUILT_SYSTEMIMAGE): $(KERNEL_MODULES_DEP)
+
+# rules to get source of Broadcom 802.11a/b/g/n hybrid device driver
+# based on broadcomsetup.sh of Kyle Evans
+WL_PATH := $(KERNEL_DIR)/drivers/net/wireless/wl
+WL_ENABLED := $(if $(wildcard $(WL_PATH)),$(shell grep ^CONFIG_WL=[my] $(KERNEL_CONFIG_FILE)))
+WL_ARCH_CHANGED := $(if $(shell file $(WL_PATH)/lib/wlc_hybrid.o_shipped | grep -s $(if $(filter x86,$(TARGET_KERNEL_ARCH)),80386,x86-64)),,FORCE)
+WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_248.tar.gz
+$(WL_SRC):
+	@echo Downloading $(@F)...
+	$(hide) curl http://www.broadcom.com/docs/linux_sta/$(@F) > $@
+$(WL_PATH)/Makefile : $(WL_SRC) $(wildcard $(WL_PATH)/*.patch) $(WL_ARCH_CHANGED) $(KERNEL_ARCH_CHANGED)
+	$(hide) tar zxf $< -C $(@D) --overwrite && \
+		patch -p5 -d $(@D) -i wl.patch && \
+		patch -p1 -d $(@D) -i linux-recent.patch
+$(INSTALLED_KERNEL_TARGET): $(if $(WL_ENABLED),$(WL_PATH)/Makefile)
+
+installclean: FILES += $(KBUILD_OUTPUT) $(INSTALLED_KERNEL_TARGET)
+
+TARGET_PREBUILT_KERNEL := $(INSTALLED_KERNEL_TARGET)
+
+.PHONY: kernel $(if $(KERNEL_ARCH_CHANGED),$(KERNEL_HEADERS_COMMON)/linux/binder.h)
+kernel: $(TARGET_PREBUILT_KERNEL)
+
+else
+
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(copy-file-to-new-target)
+ifdef TARGET_PREBUILT_MODULES
+	mkdir -p $(TARGET_OUT)/lib
+	$(hide) cp -r $(TARGET_PREBUILT_MODULES) $(TARGET_OUT)/lib
+endif
+
+endif # TARGET_PREBUILT_KERNEL
+endif # KBUILD_OUTPUT

millosr/android_build

Change-Id: I6b67229c735dacc73fe83a2a9937abdbf4a0c423

core/tasks/kernel.mk
@@ -1,0 +1,107 @@
+#
+# Copyright (C) 2014-2016 The Android-x86 Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+
+ifeq ($(KBUILD_OUTPUT),)
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+
+KERNEL_DIR ?= kernel
+
+ifneq ($(filter x86%,$(TARGET_ARCH)),)
+TARGET_KERNEL_ARCH ?= $(TARGET_ARCH)
+KERNEL_TARGET := bzImage
+TARGET_KERNEL_CONFIG ?= android-$(TARGET_KERNEL_ARCH)_defconfig
+KERNEL_CONFIG_DIR := arch/x86/configs
+endif
+ifeq ($(TARGET_ARCH),arm)
+KERNEL_TARGET := zImage
+TARGET_KERNEL_CONFIG ?= goldfish_defconfig
+KERNEL_CONFIG_DIR := arch/arm/configs
+endif
+
+KBUILD_OUTPUT := $(abspath $(TARGET_OUT_INTERMEDIATES)/kernel)
+mk_kernel := + $(hide) $(MAKE) -C $(KERNEL_DIR) O=$(KBUILD_OUTPUT) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(abspath $(TARGET_TOOLS_PREFIX)) $(if $(SHOW_COMMANDS),V=1)
+
+KERNEL_CONFIG_FILE := $(if $(wildcard $(TARGET_KERNEL_CONFIG)),$(TARGET_KERNEL_CONFIG),$(KERNEL_DIR)/$(KERNEL_CONFIG_DIR)/$(TARGET_KERNEL_CONFIG))
+
+MOD_ENABLED := $(shell grep ^CONFIG_MODULES=y $(KERNEL_CONFIG_FILE))
+FIRMWARE_ENABLED := $(shell grep ^CONFIG_FIRMWARE_IN_KERNEL=y $(KERNEL_CONFIG_FILE))
+
+# I understand Android build system discourage to use submake,
+# but I don't want to write a complex Android.mk to build kernel.
+# This is the simplest way I can think.
+KERNEL_DOTCONFIG_FILE := $(KBUILD_OUTPUT)/.config
+KERNEL_ARCH_CHANGED := $(if $(filter 0,$(shell grep -s ^$(if $(filter x86,$(TARGET_KERNEL_ARCH)),\#.)CONFIG_64BIT $(KERNEL_DOTCONFIG_FILE) | wc -l)),FORCE)
+$(KERNEL_DOTCONFIG_FILE): $(KERNEL_CONFIG_FILE) $(wildcard $(TARGET_KERNEL_DIFFCONFIG)) $(KERNEL_ARCH_CHANGED)
+	$(hide) mkdir -p $(@D) && cat $(wildcard $^) > $@
+	$(mk_kernel) oldnoconfig
+
+# bison is needed to build kernel and external modules from source
+BISON := $(HOST_OUT_EXECUTABLES)/bison$(HOST_EXECUTABLE_SUFFIX)
+
+BUILT_KERNEL_TARGET := $(KBUILD_OUTPUT)/arch/$(TARGET_ARCH)/boot/$(KERNEL_TARGET)
+$(INSTALLED_KERNEL_TARGET): $(KERNEL_DOTCONFIG_FILE) | $(ACP) $(BISON)
+	$(mk_kernel) $(KERNEL_TARGET) $(if $(MOD_ENABLED),modules)
+	$(hide) $(ACP) -fp $(BUILT_KERNEL_TARGET) $@
+	$(if $(FIRMWARE_ENABLED),$(mk_kernel) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) firmware_install)
+
+ifneq ($(MOD_ENABLED),)
+KERNEL_MODULES_DEP := $(firstword $(wildcard $(TARGET_OUT)/lib/modules/*/modules.dep))
+KERNEL_MODULES_DEP := $(if $(KERNEL_MODULES_DEP),$(KERNEL_MODULES_DEP),$(TARGET_OUT)/lib/modules)
+
+$(TARGET_OUT_INTERMEDIATES)/%.kmodule: $(INSTALLED_KERNEL_TARGET)
+	$(hide) cp -an $(EXTRA_KERNEL_MODULE_PATH_$*) $(TARGET_OUT_INTERMEDIATES)/$*.kmodule
+	@echo Building additional kernel module $*
+	$(mk_kernel) M=$(abspath $@) modules
+
+$(KERNEL_MODULES_DEP): $(INSTALLED_KERNEL_TARGET) $(patsubst %,$(TARGET_OUT_INTERMEDIATES)/%.kmodule,$(TARGET_EXTRA_KERNEL_MODULES))
+	$(hide) rm -rf $(TARGET_OUT)/lib/modules
+	$(mk_kernel) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) modules_install
+	+ $(hide) for kmod in $(TARGET_EXTRA_KERNEL_MODULES) ; do \
+	        echo Installing additional kernel module $${kmod} ; \
+	        $(subst +,,$(subst $(hide),,$(mk_kernel))) INSTALL_MOD_PATH=$(abspath $(TARGET_OUT)) M=$(abspath $(TARGET_OUT_INTERMEDIATES))/$${kmod}.kmodule modules_install ; \
+	done
+	$(hide) rm -f $(TARGET_OUT)/lib/modules/*/{build,source}
+endif
+
+$(BUILT_SYSTEMIMAGE): $(KERNEL_MODULES_DEP)
+
+# rules to get source of Broadcom 802.11a/b/g/n hybrid device driver
+# based on broadcomsetup.sh of Kyle Evans
+WL_PATH := $(KERNEL_DIR)/drivers/net/wireless/wl
+WL_ENABLED := $(if $(wildcard $(WL_PATH)),$(shell grep ^CONFIG_WL=[my] $(KERNEL_CONFIG_FILE)))
+WL_ARCH_CHANGED := $(if $(shell file $(WL_PATH)/lib/wlc_hybrid.o_shipped | grep -s $(if $(filter x86,$(TARGET_KERNEL_ARCH)),80386,x86-64)),,FORCE)
+WL_SRC := $(WL_PATH)/hybrid-v35$(if $(filter x86,$(TARGET_KERNEL_ARCH)),,_64)-nodebug-pcoem-6_30_223_271.tar.gz
+$(WL_SRC):
+	@echo Downloading $(@F)...
+	$(hide) curl http://www.broadcom.com/docs/linux_sta/$(@F) > $@
+$(WL_PATH)/Makefile : $(WL_SRC) $(wildcard $(WL_PATH)/*.patch) $(WL_ARCH_CHANGED) $(KERNEL_ARCH_CHANGED)
+	$(hide) tar zxf $< -C $(@D) --overwrite && \
+		patch -p5 -d $(@D) -i wl.patch && \
+		patch -p1 -d $(@D) -i linux-recent.patch
+$(INSTALLED_KERNEL_TARGET): $(if $(WL_ENABLED),$(WL_PATH)/Makefile)
+
+installclean: FILES += $(KBUILD_OUTPUT) $(INSTALLED_KERNEL_TARGET)
+
+TARGET_PREBUILT_KERNEL := $(INSTALLED_KERNEL_TARGET)
+
+.PHONY: kernel $(if $(KERNEL_ARCH_CHANGED),$(KERNEL_HEADERS_COMMON)/linux/binder.h)
+kernel: $(TARGET_PREBUILT_KERNEL)
+
+else
+
+$(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(copy-file-to-new-target)
+ifdef TARGET_PREBUILT_MODULES
+	mkdir -p $(TARGET_OUT)/lib
+	$(hide) cp -r $(TARGET_PREBUILT_MODULES) $(TARGET_OUT)/lib
+endif
+
+endif # TARGET_PREBUILT_KERNEL
+endif # KBUILD_OUTPUT