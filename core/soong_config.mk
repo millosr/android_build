905e6bdc68f8101688efae448c7f4fa2f1963cb5
mickybart <mickybart@pygoscelis.org>
2017-10-31 15:41:15 -0400

    soong_config: Add TARGET_HAS_FMRADIO_BCM variable

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -90,6 +90,7 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))",'; \
 	echo '    "Uses_qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
+	echo '    "Has_fmradio_bcm": $(if $(filter true,$(TARGET_HAS_FMRADIO_BCM)),true,false),'; \
 	echo ''; \
 	echo '    "ArtUseReadBarrier": $(if $(filter false,$(PRODUCT_ART_USE_READ_BARRIER)),false,true),'; \
 	echo ''; \
